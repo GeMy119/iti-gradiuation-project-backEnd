a235f01a202e0cdfab6459fb09e98f8d83fc91d7
Mohamed-Yousry-Ali <mohamedyousry631@gmail.com>
2023-11-03 22:23:14 +0200

    Finish car and resturent and visit place

--- connectionDB/car.schema.js
+++ connectionDB/car.schema.js
@@ -4,9 +4,11 @@ const carSchema = new mongoose.Schema({
     email: { type: String, required: true },
     address: { type: String, required: true },
     phone: { type: String, required: true },
+    image: { type: String },
     location: { type: String, required: true },
     rate: { type: Number},
     counter: { type: Number},
+       deleted: { type: Boolean, default: false }
 }, {
     timestamps: true
 });
--- connectionDB/visitplace.schema.js
+++ connectionDB/visitplace.schema.js
@@ -4,10 +4,12 @@ const visitSchema = new mongoose.Schema({
     email: { type: String, required: true },
     address: { type: String, required: true },
     phone: { type: String, required: true },
+    image: { type: String },
     location: { type: String, required: true },
     price: { type: Number},
     rate: { type: Number},
     counter: { type: Number},
+    deleted: { type: Boolean, default: false },
 }, {
     timestamps: true
 });
