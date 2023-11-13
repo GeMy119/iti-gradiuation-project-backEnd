54f19fd746bd9888849a0d27067f8ad66dac632e
GeMy119 <76409042+GeMy119@users.noreply.github.com>
2023-11-13 22:43:06 +0200

    edit service endpoint

--- modules/users/controller/services.controller.js
+++ modules/users/controller/services.controller.js
@@ -12,10 +12,13 @@ const addHotelId = async (req, res) => {
         if (id) {
             const userReq = req.user;
             const user = await User.findById(userReq.id);
-            console.log(user);
-            user.hotelId.push(id);
-            await user.save();
-            res.status(StatusCodes.OK).json({ message: `hotel id added` });
+            if (!user.hotelId.includes(id)) {
+                user.hotelId.push(id);
+                await user.save();
+                res.status(StatusCodes.OK).json({ message: `hotel id added` });
+            } else {
+                res.status(StatusCodes.FORBIDDEN).json({ message: `hotel id is already added` });
+            }
         } else {
             res.status(StatusCodes.BAD_REQUEST).json({ message: "Invalid ID" });
         }
@@ -26,16 +29,17 @@ const addHotelId = async (req, res) => {
 };
 const addEventId = async (req, res) => {
     try {
-        const {id} = req.body;
+        const { id } = req.body;
         if (id) {
             const userReq = req.user;
             const user = await User.findById(userReq.id)
-            // const event = await Event.findById(id)
-            user.eventId.push(id);
-            // event.counter += 1
-            // await event.save()
-            await user.save();
-            res.status(StatusCodes.OK).json({ message: `event id added` });
+            if (!user.eventId.includes(id)) {
+                user.eventId.push(id);
+                await user.save();
+                res.status(StatusCodes.OK).json({ message: `event id added` });
+            } else {
+                res.status(StatusCodes.FORBIDDEN).json({ message: `event id is already added` });
+            }
         } else {
             res.status(StatusCodes.BAD_REQUEST).json({ message: "Invalid ID" });
         }
@@ -46,15 +50,20 @@ const addEventId = async (req, res) => {
 }
 const addResId = async (req, res) => {
     try {
-        const {id} = req.body;
+        const { id } = req.body;
         if (id) {
             const userReq = req.user;
             const user = await User.findById(userReq.id)
             // const restaurant = await Resturant.findById(id)
-            user.restaurantId.push(id);
+            if (!user.restaurantId.includes(id)) {
+                user.restaurantId.push(id);
+                await user.save();
+                res.status(StatusCodes.OK).json({ message: `restaurant id added` });
+            } else {
+                res.status(StatusCodes.FORBIDDEN).json({ message: `restaurant id is already added` });
+            }
             // restaurant.counter += 1
             // await restaurant.save()
-            await user.save();
             res.status(StatusCodes.OK).json({ message: `restaurant id added` });
         } else {
             res.status(StatusCodes.BAD_REQUEST).json({ message: "Invalid ID" });
@@ -66,16 +75,19 @@ const addResId = async (req, res) => {
 }
 const addVisitPlaceId = async (req, res) => {
     try {
-        const {id} = req.body;
+        const { id } = req.body;
         if (id) {
             const userReq = req.user;
-            const user = await User.findById(userReq.id)
-            // const visitPlace = await VisitPlace.findById(id)
-            user.visitPlaceId.push(id);
-            // visitPlace.counter += 1
-            // await visitPlace.save()
-            await user.save();
-            res.status(StatusCodes.OK).json({ message: `visitPlace id added` });
+            const user = await User.findById(userReq.id);
+
+            // Check if id already exists in visitPlaceId array
+            if (!user.visitPlaceId.includes(id)) {
+                user.visitPlaceId.push(id);
+                await user.save();
+                res.status(StatusCodes.OK).json({ message: `visitPlace id added` });
+            } else {
+                res.status(StatusCodes.FORBIDDEN).json({ message: `visitPlace id is already added` });
+            }
         } else {
             res.status(StatusCodes.BAD_REQUEST).json({ message: "Invalid ID" });
         }
@@ -83,19 +95,20 @@ const addVisitPlaceId = async (req, res) => {
         console.error(error);
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Error", error });
     }
-}
+};
 const addCarId = async (req, res) => {
     try {
-        const {id} = req.body;
+        const { id } = req.body;
         if (id) {
             const userReq = req.user;
             const user = await User.findById(userReq.id)
-            // const car = await Car.findById(id)
-            user.carId.push(id);
-            // car.counter += 1
-            // await car.save()
-            await user.save();
-            res.status(StatusCodes.OK).json({ message: `car id added` });
+            if (!user.carId.includes(id)) {
+                user.carId.push(id);
+                await user.save();
+                res.status(StatusCodes.OK).json({ message: `car id added` });
+            } else {
+                res.status(StatusCodes.FORBIDDEN).json({ message: `car id is already added` });
+            }
         } else {
             res.status(StatusCodes.BAD_REQUEST).json({ message: "Invalid ID" });
         }
