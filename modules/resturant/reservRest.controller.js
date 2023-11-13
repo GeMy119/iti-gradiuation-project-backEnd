14678bf46db3ba3f9c5c408cff947349aa714436
GeMy119 <76409042+GeMy119@users.noreply.github.com>
2023-11-14 01:53:29 +0200

    added reserve

--- modules/resturant/reservRest.controller.js
+++ modules/resturant/reservRest.controller.js
@@ -34,7 +34,7 @@ const createRestReserve = async (req, res) => {
 // Controller to get all restaurant reservations
 const getAllRestReserv = async (req, res) => {
     try {
-        const restaurantReservations = await reservRest.find().populate('user').populate('Resturant');
+        const restaurantReservations = await reservRest.find().populate('user').populate('resturant');
         res.status(StatusCodes.OK).json({ restaurantReservations });
     } catch (error) {
         console.error(error);
